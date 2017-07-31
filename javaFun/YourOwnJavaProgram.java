b0e41c2c60511135060e8031b153d4bc7f7fb59e
Himanshu <himanshuguptams@gmail.com>
2017-07-31 15:56:58 -0700

    1

---
+++ javaFun/YourOwnJavaProgram.java
@@ -0,0 +1,7 @@
+public class YourOwnJavaProgram {
+    public static void main(String[] args) {
+        System.out.println("My name is Himanshu Gupta");
+        System.out.println("I am 32 years old");
+        System.out.println("My hometown is Pleasanton CA");
+    }
+}
