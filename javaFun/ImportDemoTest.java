e45b0d5c0dc034d81a02472d0739cfc15b428f4e
Himanshu <himanshuguptams@gmail.com>
2017-08-02 18:12:21 -0700

    2

---
+++ javaFun/ImportDemoTest.java
@@ -0,0 +1,7 @@
+public class ImportDemoTest{
+    public static void main(String[] args){
+        ImportDemo iD = ImportDemo();
+        String currenDate = iD.getCurrentDate();
+        System.out.println(currenDate);
+    }
+}
