0f0c7ab1d1a6c72c223a3f9d6793a087f73f8816
Himanshu <himanshuguptams@gmail.com>
2017-08-09 15:40:12 -0700

    7

---
+++ javaFun/StringManipulator.java
@@ -0,0 +1,30 @@
+public class StringManipulator {
+  // public void stringManipulatorMethod() {
+  //
+  // }
+  public String trimAndConcat(String s1, String s2){
+    s1 = s1.trim();
+    s2 = s2.trim();
+    String s = s1 +" "+ s2;
+    return s;
+  }
+  public Integer getIndexOrNull(String s3, char c1) {
+    int a = s3.indexOf(c1);
+    if (a == -1) {
+      return null;
+    }
+    else {
+      return a;
+    }
+  }
+  public Integer getIndexOrNull(String s4, String s5){
+    int b = s4.indexOf(s5);
+    if (b == -1) {
+      return null;
+    }
+    else {
+      return b;
+    }
+  }
+  public String concatSubstring(String, int, int, String)
+}
