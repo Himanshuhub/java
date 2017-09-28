2af205419908c96e514a8125344c3f869d2226c2
Himanshu <himanshuguptams@gmail.com>
2017-09-28 10:49:37 -0700

    3

--- javaFun/BasicJava.java
+++ javaFun/BasicJava.java
@@ -31,6 +31,7 @@ class BasicJava {
   }
   void static max() {
 
+
   }
 
 }
---
+++ javaFun/Simple.java
@@ -0,0 +1,8 @@
+public class Simple{
+public static void main(String args[]){
+System.out.println("hello javatpoint");
+int ourInt;
+String ourString = "Hello World";
+// ourInt = "Hello World"
+}
+}
